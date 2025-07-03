5696a01dc8b7b07b9c6659dabbca54a8c938db5a
maria147147 <maferfelicissimo1990@gmail.com>
2025-07-03 18:41:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,46 @@
+// api/index.js (ou seu entrypoint)
+
 const express = require("express");
 const mongoose = require("mongoose");
-const server = express();
-const produtoRoute = require("./routes/RoutesApp");
 const cors = require("cors");
-server.use(cors());
-//middleware
-server.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+const serverless = require("serverless-http");
+const produtoRoute = require("./routes/RoutesApp");
 
-server.use(express.json());
+const app = express();
 
-server.use("/produto", produtoRoute);
-server.use("/", produtoRoute);
-server.use("/produto/:_id", produtoRoute);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const DB_USER = "mafer";
-const DB_PASSWORD = encodeURIComponent("trabalhoweb");
+// Middleware para conexão Mongo com cache
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  const DB_USER = "mafer";
+  const DB_PASSWORD = encodeURIComponent("trabalhoweb");
+  const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@produtos.edwr37u.mongodb.net/?retryWrites=true&w=majority&appName=produtos`;
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  isConnected = true;
+  console.log("Conectado ao Mongo!");
+}
 
-mongoose
-  .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@produtos.edwr37u.mongodb.net/?retryWrites=true&w=majority&appName=produtos`
-  )
-  .then(() => {
-    console.log("Conectado ao Mongo!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-server.listen(3000);
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    console.error("Erro na conexão com MongoDB:", err);
+    res.status(500).json({ error: "Erro de conexão com banco" });
+  }
+});
+
+// Rotas
+app.use("/produto", produtoRoute);
+
+// Rota raiz (opcional)
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API rodando" });
+});
+
+// Exporta como função serverless para Vercel
+module.exports = serverless(app);
